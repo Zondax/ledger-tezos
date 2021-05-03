f028b5d1c5b2c141f0590777d4e8e8c46b26b95b
becominginsane <francesco@zondax.ch>
2021-05-03 22:31:10 +0200

    fix(buffer): fix macro scope
    fix(buffer): fix bug in creation of slice when writing to ram incrementally
    tests(buffer): add unit tests

--- app/src/bolos/buffer.rs
+++ app/src/bolos/buffer.rs
@@ -1,5 +1,7 @@
 use super::{nvm::NVM, PIC};
+use std::prelude::v1::*;
 
+#[derive(Debug, Clone, Copy)]
 enum BufferState {
     WritingToRam(usize),
     WritingToFlash(usize),
@@ -24,6 +26,20 @@ impl BufferState {
     }
 }
 
+#[cfg(test)]
+impl BufferState {
+    const fn is_ram(&self) -> bool {
+        match self {
+            Self::WritingToRam(_) => true,
+            _ => false,
+        }
+    }
+
+    const fn is_flash(&self) -> bool {
+        !self.is_ram()
+    }
+}
+
 /// This struct is used to manage 2 buffers, with one "working" buffer
 /// and a "fallback" buffer when the first one is too small for the attempted operation
 pub struct SwappingBuffer<'r, 'f, const RAM: usize, const FLASH: usize> {
@@ -42,10 +58,19 @@ impl<'r, 'f, const RAM: usize, const FLASH: usize> SwappingBuffer<'r, 'f, RAM, F
         }
     }
 
+    /// Will return the entire underlying buffer as an immutable slice
     pub fn read(&self) -> &[u8] {
         match self.state {
             BufferState::WritingToRam(_) => &self.ram[..],
             BufferState::WritingToFlash(_) => self.flash,
+        }
+    }
+
+    /// Will return the underlying written buffer as an immutable slice
+    pub fn read_exact(&self) -> &[u8] {
+        match self.state {
+            BufferState::WritingToRam(cnt) => &self.ram[..cnt],
+            BufferState::WritingToFlash(cnt) => &self.flash[..cnt],
         }
     }
 
@@ -75,7 +100,7 @@ impl<'r, 'f, const RAM: usize, const FLASH: usize> SwappingBuffer<'r, 'f, RAM, F
             //writing to ram and we have space
             BufferState::WritingToRam(cnt) => {
                 //copy slice and update counter
-                self.ram[*cnt..len].copy_from_slice(bytes);
+                self.ram[*cnt..*cnt + len].copy_from_slice(bytes);
                 *cnt += len;
                 Ok(())
             }
@@ -92,14 +117,28 @@ impl<'r, 'f, const RAM: usize, const FLASH: usize> SwappingBuffer<'r, 'f, RAM, F
     }
 
     /// Reset the buffer counter and state to the initial configuration
+    ///
+    /// # Warning
+    /// Will not overwrite the buffer contents
     pub fn reset(&mut self) {
         self.state = Default::default();
     }
 }
 
+#[cfg(test)]
+impl<'r, 'f, const RAM: usize, const FLASH: usize> SwappingBuffer<'r, 'f, RAM, FLASH> {
+    const fn sizes(&self) -> (usize, usize) {
+        (RAM, FLASH)
+    }
+
+    fn state(&self) -> BufferState {
+        self.state
+    }
+}
+
 #[macro_export]
 macro_rules! new {
-    ($ram:expr, $flash:expr) => {
+    ($ram:expr, $flash:expr) => {{
         use super::*;
         static mut RAM: PIC<[u8; $ram]> = PIC::new([0; $ram]);
 
@@ -107,5 +146,126 @@ macro_rules! new {
         static mut FLASH: PIC<NVM<$flash>> = PIC::new(NVM::new());
 
         unsafe { SwappingBuffer::new(&mut RAM, &mut FLASH) }
-    };
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const MSG: &[u8] = b"deadbeef";
+
+    #[test]
+    fn macro_works() {
+        let buffer = new!(1, 2);
+
+        assert_eq!((1, 2), buffer.sizes());
+        assert!(buffer.state.is_ram());
+    }
+
+    #[test]
+    fn no_ram() {
+        let mut buffer = new!(0, 8);
+
+        //should be able to write
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_flash()); //and be in flash
+
+        //should be readable
+        assert_eq!(MSG, buffer.read());
+
+        //full
+        assert!(buffer.write(MSG).is_err())
+    }
+
+    #[test]
+    fn no_flash() {
+        let mut buffer = new!(8, 0);
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_ram()); //should all be in ram
+
+        //should be readable
+        assert_eq!(MSG, buffer.read());
+
+        //full
+        assert!(buffer.write(MSG).is_err());
+    }
+
+    #[test]
+    fn incremental_ram() {
+        let mut buffer = new!(16, 0);
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_ram());
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_ram());
+
+        assert!(buffer.write(MSG).is_err());
+
+        //find first occurence of MSG in buffer
+        assert!(buffer
+            .read()
+            .windows(MSG.len())
+            .position(|w| w == MSG)
+            .is_some())
+    }
+
+    #[test]
+    fn incremental_flash() {
+        let mut buffer = new!(0, 16);
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_flash());
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_flash());
+
+        assert!(buffer.write(MSG).is_err());
+
+        //should find only MSG in the buffer
+        assert!(buffer.read().chunks(MSG.len()).all(|c| c == MSG))
+    }
+
+    #[test]
+    fn transition() {
+        let mut buffer = new!(4, 8);
+
+        //write 8 bytes
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_flash()); //should move to second buffer
+
+        assert!(buffer.write(MSG).is_err());
+
+        assert_eq!(MSG, buffer.read());
+    }
+
+    #[test]
+    #[should_panic]
+    fn not_enough_space() {
+        let mut buffer = new!(4, 7);
+
+        //writing 8 bytes will try to write to second buffer
+        // but will fail since no space there either
+        buffer.write(MSG).unwrap();
+    }
+
+    #[test]
+    fn reset() {
+        let mut buffer = new!(8, 16);
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_ram());
+
+        buffer.write(MSG).unwrap();
+        assert!(buffer.state.is_flash());
+
+        //check if all chunks are MSG
+        assert!(buffer.read().chunks(MSG.len()).all(|c| c == MSG));
+
+        buffer.reset();
+
+        assert!(buffer.state.is_ram());
+        assert!(buffer.read_exact().is_empty());
+    }
 }
