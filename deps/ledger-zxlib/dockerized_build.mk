1144f5fce6df1e018b6a3a2fe76fdb63b81c80cd
becominginsane <francesco@zondax.ch>
2021-05-21 03:11:57 +0200

    feat(zemu): add debug command to launch gdbserver
    feat(make): add `zemu_debug`

--- deps/ledger-zxlib/dockerized_build.mk
+++ deps/ledger-zxlib/dockerized_build.mk
@@ -274,6 +274,10 @@ zemu_debug:
 zemu_test:
 	cd $(TESTS_ZEMU_DIR) && yarn test$(COIN)
 
+.PHONY: zemu_debug
+zemu_debug:
+	cd $(TESTS_ZEMU_DIR) && yarn run debug
+
 .PHONY: rust_test
 rust_test:
 	$(MAKE) -C rust test
