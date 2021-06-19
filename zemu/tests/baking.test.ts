2408bd38d6097423015dd64f90cdfbb719052672
becominginsane <francesco@zondax.ch>
2021-06-19 02:20:05 +0200

    test(zemu): drive sign ui

--- zemu/tests/baking.test.ts
+++ zemu/tests/baking.test.ts
@@ -218,7 +218,15 @@ describe.each(models)('Standard baking [%s]; sign', function (m) {
       await sim.start({ ...defaultOptions, model: m.name })
       const app = new TezosApp(sim.getTransport())
 
-      const resp = await app.sign(APP_DERIVATION, curve, msg);
+      const respReq = app.sign(APP_DERIVATION, curve, msg);
+
+      await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot(), 20000);
+      if (m.name == "nanox") {
+          sim.clickRight();
+      }
+      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-sign-${msg.length}`, 2);
+
+      const resp = await respReq;
 
       console.log(resp, m.name)
 
