50379a51c98d15366150aa505ff8b01ab145d7b5
becominginsane <francesco@zondax.ch>
2021-06-24 14:04:19 +0200

    feat: add `LegacyPacketType`
    refactor: `PacketType` trait

---
+++ rust/app/src/handlers/utils.rs
@@ -0,0 +1,91 @@
+use crate::constants::ApduError;
+
+#[repr(u8)]
+pub enum ZPacketType {
+    Init = 0,
+    Add = 1,
+    Last = 2,
+}
+
+impl std::convert::TryFrom<u8> for ZPacketType {
+    type Error = ();
+
+    fn try_from(from: u8) -> Result<Self, ()> {
+        match from {
+            0 => Ok(Self::Init),
+            1 => Ok(Self::Add),
+            2 => Ok(Self::Last),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Into<u8> for ZPacketType {
+    fn into(self) -> u8 {
+        self as _
+    }
+}
+
+#[repr(u8)]
+pub enum LegacyPacketType {
+    Init = 0,
+    Add = 1,
+    HashOnlyNext = 3,
+    InitAndLast = 0x80,
+    AddAndLast = 0x81,
+    HashAndLast = 0x83,
+}
+
+impl std::convert::TryFrom<u8> for LegacyPacketType {
+    type Error = ();
+
+    fn try_from(from: u8) -> Result<Self, ()> {
+        match from {
+            0 => Ok(Self::Init),
+            1 => Ok(Self::Add),
+            3 => Ok(Self::HashOnlyNext),
+            0x80 => Ok(Self::InitAndLast),
+            0x81 => Ok(Self::AddAndLast),
+            0x83 => Ok(Self::HashAndLast),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Into<u8> for LegacyPacketType {
+    fn into(self) -> u8 {
+        self as _
+    }
+}
+
+pub trait PacketType {
+    fn is_init(&self) -> bool;
+    fn is_last(&self) -> bool;
+
+    fn is_next(&self) -> bool {
+        !self.is_init() && !self.is_last()
+    }
+}
+
+impl PacketType for ZPacketType {
+    fn is_init(&self) -> bool {
+        matches!(self, Self::Init)
+    }
+
+    fn is_last(&self) -> bool {
+        matches!(self, Self::Last)
+    }
+}
+
+impl PacketType for LegacyPacketType {
+    fn is_init(&self) -> bool {
+        matches!(self, Self::Init) || matches!(self, Self::InitAndLast)
+    }
+
+    fn is_last(&self) -> bool {
+        match self {
+            Self::InitAndLast | Self::HashAndLast | Self::AddAndLast => true,
+            _ => false,
+        }
+    }
+}
