5cd71a7e720075a16766f4167a06061b39f80b54
becominginsane <francesco@zondax.ch>
2021-06-29 16:21:07 +0200

    fix: remove `extra-traits-zeroize` feature
    docs: add license to all source files
    docs: add some documentation to macros

--- rust/bolos/src/swapping_buffer.rs
+++ rust/bolos/src/swapping_buffer.rs
@@ -1,3 +1,18 @@
+/*******************************************************************************
+*   (c) 2021 Zondax GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
 use super::{
     nvm::{NVMError, NVM},
     PIC,
@@ -78,7 +93,7 @@ impl<'r, 'f, const RAM: usize, const FLASH: usize> SwappingBuffer<'r, 'f, RAM, F
     /// switching to the second buffer if needed.
     ///
     /// Will copy the first buffer into the second before switching.
-    /// Switching is permanent unless [`reset`] is called
+    /// Switching is permanent unless [`Self::reset`] is called
     ///
     /// # Errors
     /// This function will error if the second buffer is smaller than the requested amount,
