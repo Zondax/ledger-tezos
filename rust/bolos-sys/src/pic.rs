8ada4f4c5964c45e333631c22f04bb9d54a17e40
becominginsane <francesco@zondax.ch>
2021-07-23 23:20:25 +0200

    fix(bolos:docs): add `bolos` dev-dependency

--- rust/bolos-sys/src/pic.rs
+++ rust/bolos-sys/src/pic.rs
@@ -24,7 +24,7 @@ use core::ops::{Deref, DerefMut};
 ///
 /// # Example
 /// ```
-/// # use bolos_sys::pic::PIC;
+/// # use bolos::pic::PIC;
 /// //BUFFER is a `static` so we need to wrap it with PIC so it would
 /// //be accessible when running under BOLOS
 /// #[bolos::pic]
