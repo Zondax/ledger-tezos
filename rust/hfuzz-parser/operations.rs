0cd41d472d0c61a6f81e0bebc90da708a8ecc991
Natanael <neithanmo@gmail.com>
2021-10-07 18:17:03 -0600

    feat(fuzzer): Fuzzer implementation using honggfuzz-rs

---
+++ rust/hfuzz-parser/operations.rs
@@ -0,0 +1,16 @@
+use rslib::parser::operations::Operation;
+
+fn main() {
+    loop {
+        honggfuzz::fuzz!(|data: &[u8]| {
+            let mut operation = if let Ok(op) = Operation::new(data) {
+                op
+            } else {
+                return;
+            };
+
+            let encoded_ops = operation.mut_ops();
+            while let Ok(Some(_)) = encoded_ops.parse_next() {}
+        });
+    }
+}
