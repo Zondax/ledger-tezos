49ee32fb8ecd8e01628b260ec70adb2760f9fce7
Natanael <neithanmo@gmail.com>
2021-10-10 10:01:05 -0600

    fix(fuzzer): Make code simplier

--- rust/hfuzz-parser/operations.rs
+++ rust/hfuzz-parser/operations.rs
@@ -3,14 +3,10 @@ use tezos_app::parser::operations::Operation;
 fn main() {
     loop {
         honggfuzz::fuzz!(|data: &[u8]| {
-            let mut operation = if let Ok(op) = Operation::new(data) {
-                op
-            } else {
-                return;
-            };
-
-            let encoded_ops = operation.mut_ops();
-            while let Ok(Some(_)) = encoded_ops.parse_next() {}
+            if let Ok(mut op) = Operation::new(data) {
+                let encoded_ops = op.mut_ops();
+                while let Ok(Some(_)) = encoded_ops.parse_next() {}
+            }
         });
     }
 }
